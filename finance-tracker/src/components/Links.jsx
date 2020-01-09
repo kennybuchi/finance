a211ab9f90e57636216dc73e0b66c652735f4f1c
Kenneth Lee <kenny.lee@moesol.com>
2020-01-09 13:38:27 -0800

    base front-end

---
+++ finance-tracker/src/components/Links.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Collapse = styled.div.attrs({
+    className: 'collapse navbar-collapse'
+})``
+
+const List = styled.div.attrs({
+    className: 'navbar-nav mr-auto',
+})``
+
+const Item = styled.div.attrs({
+    className: 'collapse navbar-collapse',
+})``
+
+class Links extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Link to='/' className='navbar-brand'>
+                    Home
+                </Link>
+                <Collapse>
+                    <List>
+                        <Item>
+                            <Link to='/create' className='nav-link'>
+                                Create User
+                            </Link>
+                        </Item>
+                        <Item>
+                            <Link to='/' className='nav-link'>
+                                Finances
+                            </Link>
+                        </Item>
+                    </List>
+                </Collapse>
+            </React.Fragment>
+
+        )
+    }
+}
+
+export default Links
