1649a3d5e20f82a7d4f17300c082465fe87ba78e
Kenneth Lee <kenny.lee@moesol.com>
2020-01-14 12:23:56 -0800

    add user component

--- finance-tracker/src/pages/AddUser.jsx
+++ finance-tracker/src/pages/AddUser.jsx
@@ -1,10 +1,74 @@
 import React, { Component } from 'react'
+import api from '../api'
+import styled from'styled-components'
 
+const Title = styled.h1.attrs({
+    className: 'h1',
+})``
+
+const Wrapper = styled.div.attrs({
+    className: 'form-group'
+})`
+    margin: 0 30px
+`
+
+const Label = styled.label`
+    margin: 5px;
+`
+
+const InputText = styled.input.attrs({
+    className: 'form-control',
+})`
+    margin: 5px;
+`
+
+const Button = styled.button.attrs({
+    className: 'btn btn-primary',
+})`
+    margin: 15px 15px 15px 5px;
+`
+
+const CancelButton = styled.a.attrs({
+    className: 'btn btn-danger',
+})`
+    margin: 15px 15px 15px 5px;
+`
 class AddUser extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+        }
+    }
+
+    handleChangeInputName = async event => {
+        const name = event.target.value
+        this.setState({ name })
+    }
+
+    handleAddUser = async() => {
+        const { name } = this.state
+        const payload = { name, tempValue: 1234 }
+        await api.addUser(payload).then(res => {
+            this.setState({
+                name: '',
+            })
+        })
+    }
+
     render() {
+        const { name } = this.state
         return (
             <div>
-                <p>Form for adding new user</p>
+                <Wrapper>
+                    <Title>Add User</Title>
+                    <Label>Username</Label>
+                    <InputText type="text" value={name} onChange={this.handleChangeInputName}/>
+
+                    <Button onClick={this.handleAddUser}>Add User</Button>
+                    <CancelButton href={'/'}>Cancel</CancelButton>
+                </Wrapper>
+
             </div>
         )
     }
